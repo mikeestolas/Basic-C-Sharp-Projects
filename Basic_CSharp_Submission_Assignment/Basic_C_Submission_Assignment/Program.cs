01f034bf637467a7bb753f0f80ad68651e8beb05
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-07-15 21:50:00 -0400

    .

---
+++ Basic_CSharp_Submission_Assignment/Basic_C_Submission_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Basic_C_Submission_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, We're \"\accounting\" on you!");
+            Console.ReadLine();
+            
+        }
+    }
+}
