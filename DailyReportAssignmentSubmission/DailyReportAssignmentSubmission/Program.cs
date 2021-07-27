5ae0a838575a8347bfbbae902cd1f763649a565b
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-07-27 15:39:16 -0400

    resubmit

--- DailyReportAssignmentSubmission/DailyReportAssignmentSubmission/Program.cs
+++ DailyReportAssignmentSubmission/DailyReportAssignmentSubmission/Program.cs
@@ -14,16 +14,16 @@ namespace DailyReportAssignmentSubmission
             Console.WriteLine("What course are you in? ");
             String coursename = Console.ReadLine();
             Console.WriteLine("What page number?");
-            String pagenum = Console.ReadLine();
+            int pagenum = Console.ReadLine();
             Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
-            String help = Console.ReadLine();
+            bool help = Console.ReadLine();
             Console.ReadLine();
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
             String experience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            String hours = Console.ReadLine();
+            int hours = Console.ReadLine();
             Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
             Console.ReadLine();
         }
